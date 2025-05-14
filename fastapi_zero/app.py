e777ed3247269133569b59e692c962bf5829cbab
Ramayana Menezes <ramayana1970@gmail.com>
2025-05-14 13:14:50 -0400

    Adicionando o exercício da aula 02

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -6,3 +7,20 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/home', response_class=HTMLResponse)
+def home():
+    return '''
+    <html>
+        <head>
+            <title>Bem vindo ao meu Olá mundo!</title>
+        </head>
+        <body>
+            <h1>Olá Mundo!!</h1>
+            <p>
+               Pra não sofrer com a maldição...
+            </p>
+        </body>
+    </html>
+    '''
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 
+from fastapi.responses import HTMLResponse
 from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
@@ -12,3 +13,26 @@ def test_root_deve_retornar_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo!'}
+
+
+def test_home_deve_retornar_html_ola_mundo():
+    cliente = TestClient(app)
+
+    html = '''
+    <html>
+        <head>
+            <title>Bem vindo ao meu Olá mundo!</title>
+        </head>
+        <body>
+            <h1>Olá Mundo!!</h1>
+            <p>
+               Pra não sofrer com a maldição...
+            </p>
+        </body>
+    </html>
+    '''
+
+    response = cliente.get('/home')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == html
