4ece9073466ed26d2f8ebfc489d13f2443d85d38
Ramayana Menezes <ramayana1970@gmail.com>
2025-05-24 12:06:54 -0400

    Aula nr 04 completada....

---
+++ tests/test_db.py
@@ -0,0 +1,59 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fastapi_zero.models import User
+
+
+def test_create_user(session, mock_db_time_created):
+    with mock_db_time_created(model=User) as time:
+        new_user = User(
+            username='alice', email='alice@dunossauro.com', password='secret'
+        )
+
+        session.add(new_user)
+        session.commit()
+
+        user_alice = session.scalar(
+            select(User).where(User.username == 'alice')
+        )
+
+        breakpoint()
+
+    assert asdict(user_alice) == {
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@dunossauro.com',
+        'password': 'secret',
+        'created_at': time,
+        'updated_at': time,
+    }
+
+
+def test_update_user(session, mock_db_time_created):
+    with mock_db_time_created(model=User) as time:
+
+        new_user = User(
+            username='alice', email='alice@dunossauro.com', password='secret'
+        )
+
+        session.add(new_user)
+        session.commit()
+
+        user_alice = session.scalar(
+            select(User).where(User.username == 'alice')
+        )
+
+        user_alice.email = 'alice@teste'
+        session.commit()
+
+        breakpoint()
+
+    assert asdict(user_alice) == {
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@teste',
+        'password': 'secret',
+        'created_at': time,
+        'updated_at': time,
+    }
